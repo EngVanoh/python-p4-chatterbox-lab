7be8a62b94260d937002f4490e95f7aa30f121ab
EVANS KIPKORIR <EVANOCRUSE@GMAIL.COM>
2025-06-29 02:32:45 +0300

    server

--- server/models.py
+++ server/models.py
@@ -2,13 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
+
+    serialize_rules = ("-updated_at",)  # Optional: skip updated_at in JSON output
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
